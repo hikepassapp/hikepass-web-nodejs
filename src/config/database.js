d038656aae863144312405ba54dc76ff011e97e7
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2026-04-29 23:30:40 +0700

    feat: implement MySQL database configuration and containerize application with Docker Compose

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
 });
-const promisePool = pool.promise();
-module.exports = promisePool;
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to MySQL database");
+});
+module.exports = connection;
